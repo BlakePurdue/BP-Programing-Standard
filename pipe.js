4b93dd85f955819635b15397c0ed98ae76bc0e6a
BlakePurdue <40712951+BlakePurdue@users.noreply.github.com>
2018-11-12 15:04:29 +1300

    Add files via upload
    Final version of game

--- pipe.js
+++ pipe.js
@@ -1,4 +1,4 @@
-class Pipe {
+class Pipe { // Main class for the pipe , which is call on the game.html functions 
     constructor(x,y,w,h,c,xspeed){
         this.x= x;
         this.y =y;
@@ -7,13 +7,18 @@ class Pipe {
         this.c= c;
         this.xspeed =xspeed;
     }
-     pipeDraw(){
+     pipeDraw(){ // Function which draws the pipes on canvas 
         canvasContext.fillStyle = this.c;
         canvasContext.fillRect(this.x,this.y,this.w,this.h);
     }
-     pipeMove (){
-        this.x += this.xspeed;
+     pipeMove(){ // Gives the pipes movement from the right side of the screen to the left, gives the idea that player is moving however player is just jumping on spot #stich-up
+        this.x -= this.xspeed;
     }
+    score() {
+           if (this.x < 0) {
+            console.log("This is working yay!")
+            Score ++;
+          }
+        }
     
 }
-
